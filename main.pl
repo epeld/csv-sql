c6346575639605abc36f9d173f3376a285acacf8
Erik Peldan <erik.peldan@gmail.com>
2018-09-08 21:35:18 +0200

    Factor out Stdin as an argument to main

--- main.pl
+++ main.pl
@@ -5,10 +5,12 @@
 :- use_module(csv_util, [stream_csv/2]).
 :- use_module(sql, [select_fields/3]).
 
-
 main :-
+  main(user_input).
+
+main(Input) :-
+  [Tab] = "\t",
   set_stream(user_output, tty(false)),
-  stream_csv(user_input, Csv),
+  stream_csv(Input, Csv),
   select_fields(Csv, [c,b,a], CsvOut),
-  csv_write_stream(user_output, CsvOut, []),
-  format("Done~n").
+  csv_write_stream(user_output, CsvOut, [separator(Tab)]).
