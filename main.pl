b505540cdd72eac10bc6598ab4cf713cbaeb7048
Erik Peldan <erik.peldan@gmail.com>
2018-09-08 20:57:14 +0200

    Change main to read csv from stdin, write to stdout

--- main.pl
+++ main.pl
@@ -2,6 +2,13 @@
 :- module(main, [main/0]).
 :- set_prolog_flag(double_quotes, codes).
 
+:- use_module(csv_util, [stream_csv/2]).
+:- use_module(sql, [select_fields/3]).
+
 
 main :-
-  format("Hi there~n").
+  set_stream(user_output, tty(false)),
+  stream_csv(user_input, Csv),
+  select_fields(Csv, [c,b,a], CsvOut),
+  csv_write_stream(user_output, CsvOut, []),
+  format("Done~n").
