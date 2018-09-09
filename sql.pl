3a9b5bb8c31939a056775d38ff72d52e22d8f731
Erik Peldan <erik.peldan@gmail.com>
2018-09-09 16:00:50 +0200

    Parse simple select queries

--- sql.pl
+++ sql.pl
@@ -1,4 +1,50 @@
-:- module(sql, [select_fields/3]).
+:- module(sql, []).
 :- set_prolog_flag(double_quotes, codes).
 
+select_query(Fields) -->
+  "select",
+  space,
+  comma_fields(Fields),
+  space,
+  "from",
+  space,
+  "stdin".
 
+
+space --> [32].
+
+
+comma_fields(all) --> "*".
+comma_fields([Field | Rest]) -->
+  field(Field),
+  comma_fields_1(Rest).
+
+
+comma_fields_1([]) --> [].
+
+
+field([Char | Rest]) -->
+  alpha(Char),
+  field_1(Rest).
+
+
+field_1([Char | Rest]) -->
+  alpha(Char),
+  field_1(Rest).
+
+field_1([]) -->
+  [].
+
+
+alpha(Char) -->
+  {
+    alpha_char(Char)
+  },
+  [Char].
+
+
+alpha_char(Char) :-
+  member(Char, "ABCDEFGHIJKLMNOPQRSTUVWXYZ").
+
+alpha_char(Char) :-
+  member(Char, "abcdefghijklmnopqrstuvwxyz").
